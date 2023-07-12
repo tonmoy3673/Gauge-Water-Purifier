f9397647504a065b78fa63eb7736d3496cc738d5
tonmoy <tonmoy3673@gmail.com>
2023-07-12 20:51:33 +0600

    make hero section rounded

--- src/component/Control.jsx
+++ src/component/Control.jsx
@@ -1,8 +1,9 @@
+import water from "@/assets/water-lg.webp";
 const Control = () => {
   return (
-    <div className="grid lg:grid-cols-2 grid-cols-1">
+    <div className="grid lg:grid-cols-2 grid-cols-1 py-16 mt-10">
       <div></div>
-      <img></img>
+      <img src={water}></img>
     </div>
   );
 };
